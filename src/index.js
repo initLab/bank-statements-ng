28592ef4d794f6e4f1fb83854c7c402bcd0c37ba
Vencislav Atanasov <git@6bez10.info>
2025-05-03 17:13:51 +0300

    Remove XML parser

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { argv } from 'node:process';
 import { readFileWithoutBom } from './util.js';
 import { parseAndValidateStatements } from './mt940/parser.js';
 import { mapStatement as mapMt940Statement } from './mt940/mapping.js';
-import { parseStatement } from './xml/parser.js';
-import { mapStatement as mapXmlStatement } from './xml/mapping.js';
 
 export async function parseStatementsFiles(paths) {
     const result = [];
@@ -17,10 +15,6 @@ export async function parseStatementsFiles(paths) {
             for (const statement of statements) {
                 result.push(mapMt940Statement(statement));
             }
-        }
-
-        if (path.toLowerCase().endsWith('.xml')) {
-            result.push(mapXmlStatement(parseStatement(contents)));
         }
     }
 
--- src/xml/parser.js
+++
@@ -1,5 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-export const parseStatement = data => new JSDOM(data, {
-    contentType: 'text/xml',
-});
