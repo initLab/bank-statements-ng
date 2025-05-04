7cecf4db25f58a9dba5d5012029091dc8dadeadb
Vencislav Atanasov <git@6bez10.info>
2025-05-04 18:55:14 +0300

    Move processing to a new module

--- src/index.js
+++ src/index.js
@@ -1,43 +1,4 @@
 import 'dotenv/config';
-import { isFileBeingProcessed, listFiles, readFile } from './s3/index.js';
-import { getEmailAttachments } from './email/index.js';
-import { parseAndValidateStatements } from './mt940/parser.js';
-
-async function processFiles() {
-    const files = await listFiles();
-
-    for (const file of files) {
-        const key = file.Key;
-
-        if (key.endsWith('/')) {
-            continue;
-        }
-
-        const alreadyProcessing = await isFileBeingProcessed(key);
-
-        if (alreadyProcessing) {
-            console.log(`Skipping file (already tagged): ${key}`);
-            continue;
-        }
-
-        // await tagFileAsProcessing(key);
-        const contents = await readFile(key);
-        const attachments = await getEmailAttachments(contents, 'text/plain');
-
-        for (const attachment of attachments) {
-            const statements = parseAndValidateStatements(attachment.content.toString());
-
-            for (const statement of statements) {
-                // TODO
-                console.log(statement);
-                process.exit();
-            }
-        }
-
-        // await deleteFile(key);
-    }
-
-    console.log('All files processed.');
-}
+import { processFiles } from './processing/index.js';
 
 await processFiles();
