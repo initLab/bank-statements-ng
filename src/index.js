e3807ec126b566ced9f8c2f101bcfdbb2ed099b8
Vencislav Atanasov <git@6bez10.info>
2025-05-04 22:06:10 +0300

    Add sutando to main module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import 'dotenv/config';
 import { processFiles } from './processing/index.js';
+import { sutando } from 'sutando';
+import * as dbConfig from '../sutando.config.js';
+
+sutando.addConnection(dbConfig);
 
 await processFiles();
+
+await sutando.destroyAll();
